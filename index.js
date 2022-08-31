fef7cd82dd67b3e290c701c4345b03ce74e8e437
CandelaIaraMartinez <105525045+CandelaIaraMartinez@users.noreply.github.com>
2022-08-31 19:42:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-let ingresarNumero = parseInt (prompt("Ingrese un numero del 1 al 10"));
+let total = 0;
 
-while (ingresarNumero <= 10) {
-    let i = ingresarNumero + 1;
-    alert("El siguiente número es" + i);
-    ingresarNumero = parseInt (prompt("Ingrese otro numero del 1 al 10"));
+function calcularMonto(precio1, precio2){
+    let total = precio1 + precio2;
+    alert("El total de su compra es de: "+ total);
+}
+
+alert("¿Le gustaría pagar en tres cuotas sin interes?");
+let cuotas = parseInt(prompt("Ingrese 1 en caso de que su respuesta sea un si, en el caso contrario ingrese 2"));
+
+if ((cuotas == 1)){  
+        function pagoCuotas(){
+            let montoCuota = total / 3; 
+            alert("Su pago se realizara en 3 cuotas de " + montoCuota);
+        }
+} else {
+    alert("Seleccione su metodo de pago")
 }
