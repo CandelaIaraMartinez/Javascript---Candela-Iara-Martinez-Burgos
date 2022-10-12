e0c6a04848347509f89d662092ce6913c5ff31b4
CandelaIaraMartinez <105525045+CandelaIaraMartinez@users.noreply.github.com>
2022-10-12 15:59:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,33 @@
-let formulario;
+√let formulario;
 let inputNombre;
 let inputCantidad;
 const recuperarCarrito = JSON.parse(localStorage.getItem("carritoJSON")) || [];
 
-//Creación del catálogo que contiene las prendas disponibles
-class Catalogo{
-    constructor(id, nombre, precio, cantidad){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = parseInt(precio);
-        this.cantidad = parseInt(cantidad);
-    }
-
-vender(){
-    this.cantidad > 0 ? this.disminuirStock(1) : Swal.fire({ title: "Sin stock", text:"No contamos con stock del producto actualmente, por favor seleccione otro.", icon:"error", confirmButtonText: "Ok" });
-}
-
-disminuirStock = (cantidadADisminuir) =>
-    (this.cantidad = this.cantidad - cantidadADisminuir);
-aumentarStock = (cantidadAAumentar) =>
-    (this.cantidad = this.cantidad + cantidadAAumentar);
-}
-
-const catalogo = [  {id: 1, nombre: "Top Negro", precio: 700, cantidad: 10},
-                    {id: 2, nombre: "Short de Jean", precio: 900,cantidad: 23},
-                    {id: 3, nombre: "Buzo Oversize", precio: 2500, cantidad: 13}
-];
-
 const mostrarProductos = document.getElementById("mostrar-productos");
+
+let producto;
 
 //Creación de las tarjetas para cada producto
 function tarjetas(){
-    catalogo.forEach((producto) => {
-        let column = document.createElement("div");
-        column.className = "col-md-3 ml-2 mt-3";
-        column.id = `columna-${producto.id}`
-        column.innerHTML = `
-            <div class="card">
-                <div class="card-body">
-                    <p class="card-text">Nombre:<b>${producto.nombre}</b></p>
-                    <p class="card-text">Precio:<b>${producto.precio}</b></p>
-                    <p class="card-text">Cantidad:<b>${producto.cantidad}</b></p>
+fetch("./catalogo.json")
+    .then((res) => res.json())
+    .then((data) => {
+        data.forEach(producto)
+            let column = document.createElement("div");
+            column.className = "col-md-3 ml-2 mt-3";
+            column.id = `columna-${producto.id}`
+            column.innerHTML = `
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text">Nombre:<b>${producto.nombre}</b></p>
+                        <p class="card-text">Precio:<b>${producto.precio}</b></p>
+                        <p class="card-text">Cantidad:<b>${producto.cantidad}</b></p>
+                    </div>
                 </div>
-            </div>
-        `
-        mostrarProductos.append(column)
-})
-};
+            `
+            mostrarProductos.append(column)
+        })
+}
 
 function inicializarElementos() {
     formulario = document.getElementById("formulario");
@@ -147,9 +128,15 @@ function mostrarCarrito() {
     });
 }
 
-let productosJSON = JSON.stringify(catalogo);
-localStorage.setItem("productosJSON", productosJSON);
-console.log(productosJSON); 
+let productosJSON;
+
+fetch("./catalogo.json")
+    .then((resp) => resp.json())
+    .then((data) => {
+        productosJSON = JSON.stringify(data)
+        localStorage.setItem("productosJSON", productosJSON);
+        console.log(productosJSON);  
+})
 
 function actualizarCarritoStorage() {
     let carritoJSON = JSON.stringify(carrito);
